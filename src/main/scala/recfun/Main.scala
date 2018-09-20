0431f4c060366f6fff6737bf07614bb28dce9b3c
mstoyanovca <mstoyanovca@gmail.com>
2018-09-19 20:29:54 -0400

    done Pascal's Triangle

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -11,11 +11,16 @@ object Main {
   }
 
 
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
 
   def balance(chars: List[Char]): Boolean = ???
 
-  
+
   def countChange(money: Int, coins: List[Int]): Int = ???
 }
--- src/test/scala/recfun/PascalSuite.scala
+++ src/test/scala/recfun/PascalSuite.scala
@@ -1,29 +1,29 @@
 package recfun
 
-import org.scalatest.FreeSpec
+import org.scalatest.{FreeSpec, Matchers}
 
-class PascalSuite extends FreeSpec {
+class PascalSuite extends FreeSpec with Matchers {
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(Main.pascal(0, 2) === 1)
+  "Given a Pascal's Triangle" - {
+    "When the column is 0" - {
+      "Then it should return 1" in {
+        Main.pascal(0, 2) shouldBe 1
       }
     }
   }
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(Main.pascal(1, 2) === 2)
+  "Given a Pascal's Triangle" - {
+    "When the column is 1 and the row is 2" - {
+      "Then it should return 2" in {
+        Main.pascal(1, 2) shouldBe 2
       }
     }
   }
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(Main.pascal(1, 3) === 3)
+  "Given a Pascal's Triangle" - {
+    "When the column is 1 and the row is 3" - {
+      "Then it should return 3" in {
+        Main.pascal(1, 3) shouldBe 3
       }
     }
   }
