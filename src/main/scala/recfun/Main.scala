95bc3ffd3c4986e8d6cf9d70dd0e69ce78815e4d
mstoyanovca <mstoyanovca@gmail.com>
2018-09-21 21:19:07 -0400

    created pascal's triangle tail recursive function

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -10,17 +12,28 @@ object Main {
     }
   }
 
-
-  def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || c == r)
-      1
-    else
-      pascal(c - 1, r - 1) + pascal(c, r - 1)
+  // non tail recursive version:
+  def pascalNTR(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1 else pascalNTR(c - 1, r - 1) + pascalNTR(c, r - 1)
   }
 
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) return 1
+
+    @tailrec
+    def loop(c: Int, r: Int, acc: List[Int]): Int = {
+      if (r == 0)
+        acc(c) // c =< r
+      else
+        loop(c, r - 1, (0 +: acc :+ 0).take(c + 2).sliding(2).toList.map(_.sum))
+    }
+
+    // do not calculate beyond c/2 due to symmetry:
+    if (c < r / 2) loop(c, r, List(1)) else loop(r - c, r, List(1))
+
+  }
 
   def balance(chars: List[Char]): Boolean = ???
-
 
   def countChange(money: Int, coins: List[Int]): Int = ???
 }
