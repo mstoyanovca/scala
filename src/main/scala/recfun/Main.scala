c2c4f3648fc9b3afae61f7af3f0c39dd1b7c7094
mstoyanovca <mstoyanovca@gmail.com>
2018-09-23 20:27:49 -0400

    completed the coin changer

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -47,5 +47,9 @@ object Main {
     loop(chars, 0)
   }
 
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money > 0 && coins.nonEmpty) countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else 0
+  }
 }
--- src/test/scala/recfun/CountChangeSuite.scala
+++ src/test/scala/recfun/CountChangeSuite.scala
@@ -1,39 +1,53 @@
 package recfun
 
-import org.scalatest.FreeSpec
+import org.scalatest.{FreeSpec, Matchers}
 
-class CountChangeSuite extends FreeSpec {
+class CountChangeSuite extends FreeSpec with Matchers {
 
-  import Main.countChange
-
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(countChange(4, List(1, 2)) === 3)
+  "Given change of 4" - {
+    "When the list of coin denominations is (1)" - {
+      "Then the ways to give change" in {
+        Main.countChange(3, List(1)) shouldBe 1
       }
     }
   }
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(countChange(300, List(5, 10, 20, 50, 100, 200, 500)) === 1022)
+  "Given change of 3" - {
+    "When the list of coin denominations is (1, 2)" - {
+      "Then the ways to give change" in {
+        Main.countChange(3, List(1, 2)) shouldBe 2
       }
     }
   }
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(countChange(301, List(5, 10, 20, 50, 100, 200, 500)) === 0)
+  "Given change of 4" - {
+    "When the list of coin denominations is (1, 2)" - {
+      "Then the ways to give change" in {
+        Main.countChange(4, List(1, 2)) shouldBe 3
       }
     }
   }
 
-  "Given" - {
-    "When" - {
-      "Then" in {
-        assert(countChange(300, List(500, 5, 50, 100, 20, 200, 10)) === 1022)
+  "Given change of 400" - {
+    "When the list of coin denominations is (5, 10, 20, 50, 100, 200, 500)" - {
+      "Then the ways to give change" in {
+        Main.countChange(300, List(5, 10, 20, 50, 100, 200, 500)) shouldBe 1022
+      }
+    }
+  }
+
+  "Given change of 301" - {
+    "When the list of coin denominations is (5, 10, 20, 50, 100, 200, 500)" - {
+      "Then the ways to give change" in {
+        Main.countChange(301, List(5, 10, 20, 50, 100, 200, 500)) shouldBe 0
+      }
+    }
+  }
+
+  "Given change of 300" - {
+    "When the list of coin denominations is (500, 5, 50, 100, 20, 200, 10)" - {
+      "Then the ways to give change" in {
+        Main.countChange(300, List(500, 5, 50, 100, 20, 200, 10)) shouldBe 1022
       }
     }
   }
