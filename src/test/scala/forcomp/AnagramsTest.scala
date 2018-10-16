e6e29c17e30cdf8ba2fdf789fa68e5fa26ce11e9
mstoyanovca <mstoyanovca@gmail.com>
2018-10-15 22:36:53 -0400

    switched to FreeSpec

--- src/test/scala/forcomp/AnagramsTest.scala
+++ src/test/scala/forcomp/AnagramsTest.scala
@@ -1,90 +1,134 @@
 package forcomp
 
-import org.scalatest.FunSuite
-import Anagrams._
+import forcomp.Anagrams._
+import org.scalatest.{FreeSpec, Matchers}
 
-class AnagramsTest extends FunSuite {
+class AnagramsTest extends FreeSpec with Matchers {
 
-  test("wordOccurrences: abcd") {
-    assert(wordOccurrences("abcd") === List(('a', 1), ('b', 1), ('c', 1), ('d', 1)))
+  "Given the Anagrams object" - {
+    "When wordOccurrences: abcd" - {
+      "Then" in {
+        wordOccurrences("abcd") shouldBe List(('a', 1), ('b', 1), ('c', 1), ('d', 1))
+      }
+    }
   }
 
-  test("wordOccurrences: Robert") {
-    assert(wordOccurrences("Robert") === List(('b', 1), ('e', 1), ('o', 1), ('r', 2), ('t', 1)))
+  "Given the Anagrams object" - {
+    "When wordOccurrences: Robert" - {
+      "Then" in {
+        wordOccurrences("Robert") shouldBe List(('b', 1), ('e', 1), ('o', 1), ('r', 2), ('t', 1))
+      }
+    }
   }
 
-  test("sentenceOccurrences: abcd e") {
-    assert(sentenceOccurrences(List("abcd", "e")) === List(('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)))
+  "Given the Anagrams object" - {
+    "When sentenceOccurrences: abcd e" - {
+      "Then" in {
+        sentenceOccurrences(List("abcd", "e")) shouldBe List(('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1))
+      }
+    }
   }
 
-  test("dictionaryByOccurrences.get: eat") {
-    assert(dictionaryByOccurrences.get(List(('a', 1), ('e', 1), ('t', 1))).map(_.toSet) === Some(Set("ate", "eat", "tea")))
+  "Given the Anagrams object" - {
+    "When dictionaryByOccurrences.get: eat" - {
+      "Then" in {
+        dictionaryByOccurrences.get(List(('a', 1), ('e', 1), ('t', 1))).map(_.toSet) shouldBe Some(Set("ate", "eat", "tea"))
+      }
+    }
   }
 
-  test("word anagrams: married") {
-    assert(wordAnagrams("married").toSet === Set("married", "admirer"))
+  "Given the Anagrams object" - {
+    "When word anagrams: married" - {
+      "Then" in {
+        wordAnagrams("married").toSet shouldBe Set("married", "admirer")
+      }
+    }
   }
 
-  test("word anagrams: player") {
-    assert(wordAnagrams("player").toSet === Set("parley", "pearly", "player", "replay"))
+  "Given the Anagrams object" - {
+    "When word anagrams: player" - {
+      "Then" in {
+        wordAnagrams("player").toSet shouldBe Set("parley", "pearly", "player", "replay")
+      }
+    }
   }
 
-  test("subtract: lard - r") {
-    val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
-    val r = List(('r', 1))
-    val lad = List(('a', 1), ('d', 1), ('l', 1))
-    assert(subtract(lard, r) === lad)
+  "Given the Anagrams object" - {
+    "When subtract: lard - r" - {
+      "Then" in {
+        val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
+        val r = List(('r', 1))
+        val lad = List(('a', 1), ('d', 1), ('l', 1))
+        subtract(lard, r) shouldBe lad
+      }
+    }
   }
 
-  test("combinations: []") {
-    assert(combinations(Nil) === List(Nil))
+  "Given the Anagrams object" - {
+    "When combinations: []" - {
+      "Then" in {
+        combinations(Nil) shouldBe List(Nil)
+      }
+    }
   }
 
-  test("combinations: abba") {
-    val abba = List(('a', 2), ('b', 2))
-    val abbacomb = List(
-      List(),
-      List(('a', 1)),
-      List(('a', 2)),
-      List(('b', 1)),
-      List(('a', 1), ('b', 1)),
-      List(('a', 2), ('b', 1)),
-      List(('b', 2)),
-      List(('a', 1), ('b', 2)),
-      List(('a', 2), ('b', 2))
-    )
-    assert(combinations(abba).toSet === abbacomb.toSet)
+  "Given the Anagrams object" - {
+    "When combinations: abba" - {
+      "Then" in {
+        val abba = List(('a', 2), ('b', 2))
+        val abbacomb = List(
+          List(),
+          List(('a', 1)),
+          List(('a', 2)),
+          List(('b', 1)),
+          List(('a', 1), ('b', 1)),
+          List(('a', 2), ('b', 1)),
+          List(('b', 2)),
+          List(('a', 1), ('b', 2)),
+          List(('a', 2), ('b', 2))
+        )
+        assert(combinations(abba).toSet === abbacomb.toSet)
+      }
+    }
   }
 
-  test("sentence anagrams: []") {
-    val sentence = List()
-    assert(sentenceAnagrams(sentence) === List(Nil))
+  "Given the Anagrams object" - {
+    "When sentence anagrams: []" - {
+      "Then" in {
+        val sentence = List()
+        assert(sentenceAnagrams(sentence) === List(Nil))
+      }
+    }
   }
 
-  test("sentence anagrams: Linux rulez") {
-    val sentence = List("Linux", "rulez")
-    val anas = List(
-      List("Rex", "Lin", "Zulu"),
-      List("nil", "Zulu", "Rex"),
-      List("Rex", "nil", "Zulu"),
-      List("Zulu", "Rex", "Lin"),
-      List("null", "Uzi", "Rex"),
-      List("Rex", "Zulu", "Lin"),
-      List("Uzi", "null", "Rex"),
-      List("Rex", "null", "Uzi"),
-      List("null", "Rex", "Uzi"),
-      List("Lin", "Rex", "Zulu"),
-      List("nil", "Rex", "Zulu"),
-      List("Rex", "Uzi", "null"),
-      List("Rex", "Zulu", "nil"),
-      List("Zulu", "Rex", "nil"),
-      List("Zulu", "Lin", "Rex"),
-      List("Lin", "Zulu", "Rex"),
-      List("Uzi", "Rex", "null"),
-      List("Zulu", "nil", "Rex"),
-      List("rulez", "Linux"),
-      List("Linux", "rulez")
-    )
-    assert(sentenceAnagrams(sentence).toSet === anas.toSet)
+  "Given the Anagrams object" - {
+    "When sentence anagrams: Linux rulez" - {
+      "Then" in {
+        val sentence = List("Linux", "rulez")
+        val anas = List(
+          List("Rex", "Lin", "Zulu"),
+          List("nil", "Zulu", "Rex"),
+          List("Rex", "nil", "Zulu"),
+          List("Zulu", "Rex", "Lin"),
+          List("null", "Uzi", "Rex"),
+          List("Rex", "Zulu", "Lin"),
+          List("Uzi", "null", "Rex"),
+          List("Rex", "null", "Uzi"),
+          List("null", "Rex", "Uzi"),
+          List("Lin", "Rex", "Zulu"),
+          List("nil", "Rex", "Zulu"),
+          List("Rex", "Uzi", "null"),
+          List("Rex", "Zulu", "nil"),
+          List("Zulu", "Rex", "nil"),
+          List("Zulu", "Lin", "Rex"),
+          List("Lin", "Zulu", "Rex"),
+          List("Uzi", "Rex", "null"),
+          List("Zulu", "nil", "Rex"),
+          List("rulez", "Linux"),
+          List("Linux", "rulez")
+        )
+        assert(sentenceAnagrams(sentence).toSet === anas.toSet)
+      }
+    }
   }
 }
