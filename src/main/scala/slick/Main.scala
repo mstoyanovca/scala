2d4827ae62387700fbfbf02427de7d132b6bef13
mstoyanovca <mstoyanovca@gmail.com>
2018-11-30 00:19:23 -0500

    slick queries

--- src/main/scala/slick/Main.scala
+++ src/main/scala/slick/Main.scala
@@ -1,18 +1,15 @@
 package slick
 
-import slick.CoffeeRepository.getCoffeeResult
+import java.util.concurrent.TimeUnit
 import slick.dbio.DBIO
+import slick.jdbc.GetResult
 import slick.jdbc.H2Profile.api._
-
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import slick.backend.DatabasePublisher
-import slick.driver.H2Driver.api._
+import scala.concurrent.Await
+import scala.concurrent.duration.FiniteDuration
 
 object Main {
+  implicit val getCoffeeResult: AnyRef with GetResult[Coffee] = GetResult(r => Coffee(r.<<, r.<<, r.<<, r.<<, r.<<))
+
   val coffees: Seq[Coffee] = Seq(
     Coffee("Colombian", 101, 7.99, 0, 0),
     Coffee("French_Roast", 49, 8.99, 0, 0),
@@ -21,12 +18,31 @@ object Main {
     Coffee("French_Roast_Decaf", 49, 9.99, 0, 0)
   )
 
+  val suppliers: Seq[Supplier] = Seq(
+    Supplier(101, "Acme Inc.", "99 Market Street", "Groundsville", "CA", "95199"),
+    Supplier(49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460"),
+    Supplier(150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966")
+  )
+
   def main(args: Array[String]): Unit = {
     val db = Database.forConfig("h2mem")
-    // val combined: Future[Seq[Int]] = db.run(DBIO.sequence(createCoffees +: coffees.map(insert) :+ findAll))
-    val combined: Future[Int] = db.run(createCoffees andThen DBIO.sequence(coffees.map(insert)) andThen findAll)
+    Await.result(
+      db.run(createSuppliers andThen createCoffees andThen DBIO.sequence(suppliers.map(insertSupplier)) andThen DBIO.sequence(coffees.map(insertCoffee))),
+      FiniteDuration(2L, TimeUnit.SECONDS)
+    )
+    val coffeesFromDB: Vector[Coffee] = Await.result(db.run(findAllCoffees()), FiniteDuration(1L, TimeUnit.SECONDS))
+    coffeesFromDB.foreach(println)
     db.close
   }
+
+  def createSuppliers: DBIO[Int] =
+    sqlu"""create table suppliers(
+    id int not null primary key,
+    name varchar not null,
+    street varchar not null,
+    city varchar not null,
+    state varchar not null,
+    zip varchar not null)"""
 
   def createCoffees: DBIO[Int] =
     sqlu"""create table coffees(
@@ -37,11 +53,15 @@ object Main {
     total int not null,
     foreign key(sup_id) references suppliers(id))"""
 
-  def insert(c: Coffee): DBIO[Int] = {
+  def insertCoffee(c: Coffee): DBIO[Int] = {
     sqlu"insert into coffees values (${c.name}, ${c.supID}, ${c.price}, ${c.sales}, ${c.total})"
   }
 
-  def findAll(): DBIO[Int] = {
-    sqlu"select * from coffees"
+  def insertSupplier(s: Supplier): DBIO[Int] = {
+    sqlu"insert into suppliers values(${s.id}, ${s.name}, ${s.street}, ${s.city}, ${s.state}, ${s.zip})"
+  }
+
+  def findAllCoffees(): DBIO[Vector[Coffee]] = {
+    sql"select * from coffees".as[Coffee](getCoffeeResult)
   }
 }
